7a24f50971af0fab4573df101861d72e6a9aa894
Tucker Fredrickson <tuck.fredrickson@gmail.com>
2016-10-08 22:40:50 -0700

    footer add and question two formated

--- js/quiz.js
+++ js/quiz.js
@@ -1,86 +1,138 @@
 $(document).ready(function(){
-	//global
-	var questions = ["Who is Yoda's master?", 'garden', 'sea', 'cat'],
-	counter = 0;
 
 	//Question 1
     
 	//Pops up a box telling the User that they were correct and provides fact
-    $("#guessButton").click(function(){
-		var guess = $(".userGuess").text();
-			console.log(guess);
-			if (guess == "Darth Plagueis"){
-				$("#guessButton").remove();
-				$("h1").empty();
-				$("#questionOne").css("margin-top", "52px");
-				$("#absoluteMessageContainer").hide().fadeIn(1000);
-				$("#incorrectMessage").removeClass("incorrectMessage").addClass("correctMessage").empty().append("Correct !");
-				$(".fact").empty().append("Darth Plagueis was a male Dark Lord of the Sith and the master of Darth Sidious. He acquired considerable knowledge in the field of midi-chlorian manipulation, but was ultimately betrayed and murdered by his own apprentice, in accordance with the \"Rule of Two.\" Sidious later recounted the tale of his master's demise to lure Anakin Skywalker to the dark side.");
-				$("#userGuess").removeClass("userGuess").css("font-size", "2.5rem").hide().fadeIn(1000);
-				$("#guessBox").appendTo("#moveUserGuess").css({
-					"margin-top" : "0",
-					"background-color" : "rgba(255, 255, 255, 0.9)",
-					"color" : "black",
-					"border" : "3px solid black"
-				});
-				$("#answerOne").removeClass("answerOne").addClass("opacityFeature");
-				$("#answerTwo").removeClass("answerTwo").addClass("opacityFeature");
-				$("#answerThree").removeClass("answerThree").addClass("opacityFeature");
-				$("#answerFour").removeClass("answerFour").addClass("opacityFeature");
-				$("#answerFive").removeClass("answerFive").addClass("opacityFeature");
-			}
+$(function() {
+$(document).keydown(function(e) {
+	switch(e.which) { 
+	case 13: // enter key
+		$(".enterKeyPress").trigger("click")
+		break; 
+	} 
+});
+});
 
-			//Tells User they were incorrect and provides fact
-			else if (guess == "Darth Nihilus" || guess == "Exar Kun" || guess == "Valkorion" || guess == "Darth Bane") {
-				$("#guessButton").remove();
-				$("h1").empty();
-				$("#questionOne").css("margin-top", "52px");
-				$("#absoluteMessageContainer").hide().fadeIn(1000);
-				$("#userGuess").removeClass("userGuess").css("font-size", "2.5rem").hide().fadeIn(1000);
-				$("#guessBox").appendTo("#moveUserGuess").css({
-					"margin-top" : "0",
-					"background-color" : "rgba(0, 0, 0, 0.8)",
-					"color" : "white",
-					"border" : "1px solid white"
-				});
-				$("#answerOne").removeClass("answerOne").addClass("opacityFeature");
-				$("#answerTwo").removeClass("answerTwo").addClass("opacityFeature");
-				$("#answerThree").removeClass("answerThree").addClass("opacityFeature");
-				$("#answerFour").removeClass("answerFour").addClass("opacityFeature");
-				$("#answerFive").removeClass("answerFive").addClass("opacityFeature");
-			}
-		});
-
-	//Pops up a box telling the user to click on an icon when one has not been selected
-    $("#guessButton").click(function(){
+//Pops up a box telling the user to click on an icon when one has not been selected
+    $(".guessButtonOne").click(function(){
 		var guess = $(".userGuess").text();
 			if (guess == "Select Icon"){
 				$("#warningMessage").append("<span id='warningMessage' class='warningMessage'><span class='fa fa-exclamation-triangle spaceRight'></span>Please Select an Icon</span>");
 			}
 		});
 
-	//cyles through Questions Text placing them in the h1 element
-    //$("#guessButton").click(function (){
-		//var guess = $(".userGuess").text();
-		//if (counter >= questions.length || guess == "Select Icon") return; // the >= operator prevents the counter from resetting onece it cyles thorugh the whole quesiton when it reaches the length of the array.
-		//$("h1").empty().append(questions[counter]);
-		//counter = (counter + 1); // increments counter after displaying the string in position 0.
-    //});
+//Series of Events that happen when user submits answer
+
+	$(".guessButtonOne").click(function(){
+		var guess = $(".userGuess").text();
+			if (guess == "Darth Nihilus") {
+				$("#userAnswerOneText").empty().append("Darth Nihilus");
+				$("#userAnswerOne").removeClass("backgroundOpacity").addClass("darthNihilusFooterIMG");
+			}
+			else if (guess == "Darth Plagueis") {
+				$("#userAnswerOneText").empty().append("Darth Plagueis");
+				$("#userAnswerOne").removeClass("backgroundOpacity").addClass("darthPlagueisFooterIMG");
+			}
+			else if (guess == "Exar Kun") {
+				$("#userAnswerOneText").empty().append("Exar Kun");
+				$("#userAnswerOne").removeClass("backgroundOpacity").addClass("exarKunFooterIMG");
+			}
+			else if (guess == "Valkorion") {
+				$("#userAnswerOneText").empty().append("Valkorion");
+				$("#userAnswerOne").removeClass("backgroundOpacity").addClass("valkorionFooterIMG");
+			}
+			else if (guess == "Darth Bane") {
+				$("#userAnswerOneText").empty().append("Darth Bane");
+				$("#userAnswerOne").removeClass("backgroundOpacity").addClass("darthBaneFooterIMG");
+			}
+	});
+
+    $(".guessButtonOne").click(function(){
+		var guess = $(".userGuess").text();
+			if (guess == "Darth Plagueis"){
+				$(".guessButtonOne").hide();
+				$("#guessButton").removeClass("enterKeyPress");
+				$("#nextButton").addClass("enterKeyPress");
+				$("#header").empty();
+				$("#absoluteMessageContainer").hide().fadeIn(1000);
+				$("#incorrectMessage").removeClass("incorrectMessage").addClass("correctMessage").empty().append("Correct !");
+				$(".fact").empty().append("Darth Plagueis was a male Dark Lord of the Sith and the master of Darth Sidious. He acquired considerable knowledge in the field of midi-chlorian manipulation, but was ultimately betrayed and murdered by his own apprentice, in accordance with the \"Rule of Two.\" Sidious later recounted the tale of his master's demise to lure Anakin Skywalker to the dark side.");
+				$("#userGuess").removeClass("userGuess").addClass("userGuessFontChange").hide().fadeIn(1000);
+				$("#guessBox").addClass("guessBoxCorrect guessBoxOnSubmit").removeClass("guessBox").appendTo("#moveUserGuess");
+				$("#answerOne").removeClass("answerOne").addClass("opacityFeature");
+				$("#answerTwo").removeClass("answerTwo").addClass("opacityFeature");
+				$("#answerThree").removeClass("answerThree").addClass("opacityFeature");
+				$("#answerFour").removeClass("answerFour").addClass("opacityFeature");
+				$("#answerFive").removeClass("answerFive").addClass("opacityFeature");
+			}
+
+//Tells User they were incorrect and provides fact
+			else if (guess == "Darth Nihilus" || guess == "Exar Kun" || guess == "Valkorion" || guess == "Darth Bane") {
+				$(".guessButtonOne").hide();
+				$("#guessButton").removeClass("enterKeyPress");
+				$("#nextButton").addClass("enterKeyPress");
+				$("#header").empty();
+				$("#absoluteMessageContainer").hide().fadeIn(1000);
+				$("#userGuess").removeClass("userGuess").addClass("userGuessFontChange").hide().fadeIn(1000);
+				$("#guessBox").addClass("guessBoxIncorrect").appendTo("#moveUserGuess");
+				$("#answerOne").removeClass("answerOne").addClass("opacityFeature");
+				$("#answerTwo").removeClass("answerTwo").addClass("opacityFeature");
+				$("#answerThree").removeClass("answerThree").addClass("opacityFeature");
+				$("#answerFour").removeClass("answerFour").addClass("opacityFeature");
+				$("#answerFive").removeClass("answerFive").addClass("opacityFeature");
+			}
+		});
 
     //goes to question two
     $(".tooQuestionTwo").click(function(){
-    	$("#background").removeClass("background").addClass("backgroundTwo");
+		$("#nextButton").removeClass("enterKeyPress");
+		$("#guessButton").addClass("enterKeyPress");
+    	$("#absoluteMessageContainer").fadeOut(500, function(){
+    		//Answer One	
+    			$("#answerOne").removeClass("opacityFeature onClick border2").addClass("answerOne border");
+    			$("#imageOne").removeClass("darthNihilusIMG").addClass("bethAllusisIMG");
+    			$("#textOne").empty().append("Beth Allusis");
+    		//Answer Two	
+    			$("#answerTwo").removeClass("opacityFeature onClick border2").addClass("answerTwo border");
+    			$("#imageTwo").removeClass("darthPlagueisIMG").addClass("odanUrrIMG");
+    			$("#textTwo").empty().append("Odan Urr");
+    		//Answer Three	
+    			$("#answerThree").removeClass("opacityFeature onClick border2").addClass("answerThree border");
+    			$("#imageThree").removeClass("exarKunIMG").addClass("lordHothIMG");
+    			$("#textThree").empty().append("Lord Hoth");
+    		//Answer Four	
+    			$("#answerFour").removeClass("opacityFeature onClick border2").addClass("answerFour border");
+    			$("#imageFour").removeClass("valkorionIMG").addClass("delGormoIMG");
+    			$("#textFour").empty().removeClass("optionText").addClass("optionTextGormo").append("N'Kata Del Gormo");
+    		//Answer Five	
+    			$("#answerFive").removeClass("opacityFeature onClick border2").addClass("answerFive border");
+    			$("#imageFive").removeClass("darthBaneIMG").addClass("sateleShanIMG");
+    			$("#textFive").empty().addClass("letterBlack").append("Satele Shan");
+    		//Adjusting Positioning for Footer
+    			$("#questionOne").removeClass("questionOne").addClass("questionOneAdjustForFooter");
+    			$("#questionContainer").removeClass("questionContainer").addClass("questionContainerAdjustForFooter");
+    		});
+    	$("#guessBox").fadeOut(500, function(){
+    		$("#guessBox").appendTo("#guessBoxContainer").removeClass("guessBoxIncorrect guessBoxCorrect guessBoxOnSubmit").addClass("guessBoxAdjustForFooter").delay(100).fadeIn(500);
+    		});
+		$("#footer").hide().delay(600).fadeIn(500);
+    	$("#userGuess").empty().append("Select Icon").removeClass("userGuessFontChange").addClass("userGuess");
+    	$(".guessButtonOne").delay(600).fadeIn(500);
+    	$("#guessButton").removeClass("guessButtonOne").addClass("guessButtonTwo");
+    	$("#header").hide().append("Who was Yoda's Jedi Master?").delay(600).fadeIn(500);
+    	$("#answers").fadeOut(500).delay(100).fadeIn(500);
     });
 
 
 	//change classes on click
 	$(".answerOne").click(function(click){
+		var text = $("#textOne").text();
 		$(".answerOne").addClass("onClick border2").removeClass("border");
 		$(".answerTwo").removeClass("onClick border2").addClass("border");
 		$(".answerThree").removeClass("onClick border2").addClass("border");
 		$(".darthBane").removeClass("onClick border2").addClass("border");
 		$(".answerFive").removeClass("onClick border2").addClass("border");
-		$(".userGuess").empty().append("Darth Nihilus");
+		$(".userGuess").empty().append(text);
 		$("#warningMessage").remove();
 		$(".message").removeClass("fa fa-exclamation-triangle").empty();
 	});
@@ -93,12 +145,13 @@ $(document).ready(function(){
 		});
 
 	$(".answerTwo").click(function(click){
+		var text = $("#textTwo").text();
 		$(".answerTwo").addClass("onClick border2").removeClass("border");
 		$(".answerOne").removeClass("onClick border2").addClass("border");
 		$(".answerThree").removeClass("onClick border2").addClass("border");
 		$(".answerFour").removeClass("onClick border2").addClass("border");
 		$(".answerFive").removeClass("onClick border2").addClass("border");
-		$(".userGuess").empty().append("Darth Plagueis");
+		$(".userGuess").empty().append(text);
 		$("#warningMessage").remove();
 		$(".message").removeClass("fa fa-exclamation-triangle").empty();
 	});
@@ -113,12 +166,13 @@ $(document).ready(function(){
 		});
 
 	$(".answerThree").click(function(click){
+		var text = $("#textThree").text();
 		$(".answerThree").addClass("onClick border2").removeClass("border");
 		$(".answerOne").removeClass("onClick border2").addClass("border");
 		$(".answerTwo").removeClass("onClick border2").addClass("border");
 		$(".answerFour").removeClass("onClick border2").addClass("border");
 		$(".answerFive").removeClass("onClick border2").addClass("border");
-		$(".userGuess").empty().append("Exar Kun");
+		$(".userGuess").empty().append(text);
 		$("#warningMessage").remove();
 		$(".message").removeClass("fa fa-exclamation-triangle").empty();
 	});
@@ -132,12 +186,13 @@ $(document).ready(function(){
 		});
 
 	$(".answerFour").click(function(click){
+		var text = $("#textFour").text();
 		$(".answerFour").addClass("onClick border2").removeClass("border");
 		$(".answerOne").removeClass("onClick border2").addClass("border");
 		$(".answerTwo").removeClass("onClick border2").addClass("border");
 		$(".answerThree").removeClass("onClick border2").addClass("border");
 		$(".answerFive").removeClass("onClick border2").addClass("border");
-		$(".userGuess").empty().append("Valkorion");
+		$(".userGuess").empty().append(text);
 		$("#warningMessage").remove();
 		$(".message").removeClass("fa fa-exclamation-triangle").empty();
 	});
@@ -150,12 +205,13 @@ $(document).ready(function(){
 		});
 
 	$(".answerFive").click(function(click){
+		var text = $("#textFive").text();
 		$(".answerFive").addClass("onClick border2").removeClass("border");
 		$(".answerOne").removeClass("onClick border2").addClass("border");
 		$(".answerTwo").removeClass("onClick border2").addClass("border");
 		$(".answerThree").removeClass("onClick border2").addClass("border");
 		$(".answerFour").removeClass("onClick border2").addClass("border");
-		$(".userGuess").empty().append("Darth Bane");
+		$(".userGuess").empty().append(text);
 		$("#warningMessage").remove();
 		$(".message").removeClass("fa fa-exclamation-triangle").empty();
 	});
